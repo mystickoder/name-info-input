aa62e10f208bd257c52a619e9906261591724c40
mystickoder <89115482+mystickoder@users.noreply.github.com>
2021-09-30 15:14:06 -0400

    Add files via upload
    name info input

---
+++ name-info-input.py
@@ -0,0 +1,17 @@
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+locaton = input("Where do you live? ")
+birth = input("What city are you from ? ")
+
+if "name" in first_name:
+    print("Hello {first_name} ")
+
+if "name" in "location":
+    print("Oh I love {location}")
+
+if "birth list city":
+    print("Oh, I love that place")
+
+if "location" in "location":
+    print(f"{first_name} {last_name}, how long have you in lived {locaton}")
+
